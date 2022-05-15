cd6cb585ba7b62f7a022265b1fb8ba83a347dcb1
Mark Nelyubin <m.nelyubin@sredaobuchenia.ru>
2022-05-15 13:32:42 +0300

    refactor styles as css modules

--- components/Navbar/index.js
+++ components/Navbar/index.js
@@ -1,19 +1,22 @@
 import Link from "next/link";
+import styles from "./index.module.scss";
 const Navbar = () => {
   return (
-    <nav className="navbar">
+    <nav className={styles.navbar}>
       <div className="container">
-        <Link href="/">
-          <a className="title">Main</a>
-        </Link>
+        <div className={styles.navContainer}>
+          <Link href="/">
+            <a className={styles.title}>Main</a>
+          </Link>
 
-        <ul>
-          <li>
-            <Link href="/posts">
-              <a>All posts</a>
-            </Link>
-          </li>
-        </ul>
+          <ul>
+            <li>
+              <Link href="/posts">
+                <a className={styles.link}>All posts</a>
+              </Link>
+            </li>
+          </ul>
+        </div>
       </div>
     </nav>
   );
