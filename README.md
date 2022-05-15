4a19c62759a27bc360ef5c7fddb01944ce5567fe
Mark Nelyubin <m.nelyubin@sredaobuchenia.ru>
2022-05-15 12:32:55 +0300

    fetching posts from strapi

--- README.md
+++ README.md
@@ -4,4 +4,8 @@
 
 `Strapi` — headless CMS. Помогает создавать контент через админку, и получать его на фронте через REST или GraphQL. Подробнее [можно почитать на сайте](https://strapi.io/).
 
+![Strapi Admin Panel](https://raw.githubusercontent.com/strapi/strapi/0bcebf77b37182fe021cb59cc19be8f5db4a18ac/public/assets/administration_panel.png "Strapi Admin Panel")
+
+---
+
 Проект включает в себя базовые страницы и компоненты для блога на Next.js в связке с Strapi.
--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,22 @@
+import axios from "axios";
 import Head from "next/head";
 import Image from "next/image";
 
-export default function Home() {
-  return <>Home</>;
+export default function Home({ posts }) {
+  return (
+    <>
+      <h1>{posts[1].attributes.title}</h1>
+    </>
+  );
+}
+
+export async function getStaticProps() {
+  const postsRes = await axios.get("http://localhost:1337/api/posts-plural/");
+  const posts = postsRes.data.data;
+
+  return {
+    props: {
+      posts,
+    },
+  };
 }
