692752d8cfb1d58f92977f8edc5bc07cb94d7404
Mark Nelyubin <m.nelyubin@sredaobuchenia.ru>
2022-05-16 15:27:10 +0300

    Added styles to post card

--- components/PostPreview/index.js
+++ components/PostPreview/index.js
@@ -1,9 +1,11 @@
+import styles from "./post.module.scss";
+
 const PostPreview = ({ post }) => {
   return (
-    <>
+    <div className={styles.post}>
       <h3>{post.title}</h3>
       <p>{post.description}</p>
-    </>
+    </div>
   );
 };
 
