d8e95633991b2f1f7562b07457bf70aebacbf3c9
Mark Nelyubin <m.nelyubin@sredaobuchenia.ru>
2022-05-25 23:38:54 +0300

    added paths from main page depending on user authorization status

--- pages/index.js
+++ pages/index.js
@@ -7,23 +7,24 @@ import RegisterComp from "../components/RegisterComp";
 import { useRouter } from "next/router";
 import { Button } from "react-bootstrap";
 import Login from "../components/Login";
+import nookies from "nookies";
 
-export default function Home({ posts }) {
-  const [latestPosts, setLatestPosts] = useState([]);
+export default function Home() {
+  // const [latestPosts, setLatestPosts] = useState([]);
   const router = useRouter();
 
   const goToRegister = () => {
     router.push("/register");
   };
 
-  useEffect(() => {
-    setLatestPosts(posts.slice(0, 3));
-  }, [posts]);
+  // useEffect(() => {
+  //   setLatestPosts(posts.slice(0, 3));
+  // }, [posts]);
 
   return (
     <>
-      <HomeHeader />
-      <PostsList posts={latestPosts} />
+      {/* <HomeHeader />
+      <PostsList posts={latestPosts} /> */}
       <Login />
       <Button variant="dark" onClick={goToRegister}>
         Зарегистрируйтесь
@@ -32,13 +33,36 @@ export default function Home({ posts }) {
   );
 }
 
-export async function getStaticProps() {
-  const postsRes = await axios.get(`${API_URL}/api/courses/`);
-  const posts = postsRes.data.data;
+// If the user does exist, then we redirect to /profile route.
+// If not, we return an empty props object and stay on the same route to render the <LoginComponent/>
+export async function getServerSideProps(context) {
+  const cookies = nookies.get(context);
+  let user = null;
+
+  if (cookies?.jwt) {
+    try {
+      const { data } = await axios.get("http://localhost:1337/api/users/me", {
+        headers: {
+          Authorization: `Bearer ${cookies.jwt}`,
+        },
+      });
+
+      user = data;
+    } catch (error) {
+      console.log(error);
+    }
+
+    if (user) {
+      return {
+        redirect: {
+          permanent: false,
+          destination: "/profile",
+        },
+      };
+    }
+  }
 
   return {
-    props: {
-      posts,
-    },
+    props: {},
   };
 }
