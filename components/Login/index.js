8841cb0c2399780b4652ab2fd3d2c3a2307121df
Mark Nelyubin <m.nelyubin@sredaobuchenia.ru>
2022-05-26 00:06:40 +0300

    Login component

--- components/Login/index.js
+++ components/Login/index.js
@@ -1,5 +1,47 @@
+import axios from "axios";
+import { useRouter } from "next/router";
+import { useState } from "react";
+
 const Login = () => {
-  return <h1>Login</h1>;
+  const [userData, setUserData] = useState({
+    identifier: "",
+    password: "",
+  });
+
+  const router = useRouter();
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+
+    try {
+      await axios.post("/api/login", { ...userData });
+      router.push("/profile");
+    } catch (error) {
+      console.log(error.response.data);
+    }
+  };
+
+  const handleChange = (e) => {
+    console.log(e.target);
+    const { name, value } = e.target;
+    setUserData({ ...userData, [name]: value });
+  };
+  return (
+    <div>
+      <form onSubmit={handleSubmit}>
+        <label>
+          Email:
+          <input type="email" name="identifier" onChange={handleChange} />
+        </label>
+
+        <label>
+          Password:
+          <input type="password" name="password" onChange={handleChange} />
+        </label>
+        <button> Submit </button>
+      </form>
+    </div>
+  );
 };
 
 export default Login;
